16c7bf55df92319415dd605b5b6f9cc134652f89
sevmorris <7morris@gmail.com>
2022-07-29 11:30:57 -0700

    220729-1130

--- dotfiles/.zprofile
+++ dotfiles/.zprofile
@@ -28,5 +28,19 @@ case `arch` in
 esac
 export PATH="$BREW_PREFIX/bin:$PATH"
 
+# Using GNU command line tools in macOS instead of FreeBSD tools
+# https://gist.github.com/skyzyx/3438280b18e4f7c490db8a2a2ca0b9da
+BREW_BIN="/usr/local/bin/brew"
+if [ -f "/opt/homebrew/bin/brew" ]; then
+    BREW_BIN="/opt/homebrew/bin/brew"
+fi
+
+if type "${BREW_BIN}" &> /dev/null; then
+    export BREW_PREFIX="$("${BREW_BIN}" --prefix)"
+    for bindir in "${BREW_PREFIX}/opt/"*"/libexec/gnubin"; do export PATH=$bindir:$PATH; done
+    for bindir in "${BREW_PREFIX}/opt/"*"/bin"; do export PATH=$bindir:$PATH; done
+    for mandir in "${BREW_PREFIX}/opt/"*"/libexec/gnuman"; do export MANPATH=$mandir:$MANPATH; done
+    for mandir in "${BREW_PREFIX}/opt/"*"/share/man/man1"; do export MANPATH=$mandir:$MANPATH; done
+fi
 
 neofetch
--- scripts/trap.sh
+++ scripts/trap.sh
@@ -128,21 +128,6 @@ cask "raycast"
 RUBY
 echo "OK"
 
-# Using GNU command line tools in macOS instead of FreeBSD tools
-# https://gist.github.com/skyzyx/3438280b18e4f7c490db8a2a2ca0b9da
-BREW_BIN="/usr/local/bin/brew"
-if [ -f "/opt/homebrew/bin/brew" ]; then
-    BREW_BIN="/opt/homebrew/bin/brew"
-fi
-
-if type "${BREW_BIN}" &> /dev/null; then
-    export BREW_PREFIX="$("${BREW_BIN}" --prefix)"
-    for bindir in "${BREW_PREFIX}/opt/"*"/libexec/gnubin"; do export PATH=$bindir:$PATH; done
-    for bindir in "${BREW_PREFIX}/opt/"*"/bin"; do export PATH=$bindir:$PATH; done
-    for mandir in "${BREW_PREFIX}/opt/"*"/libexec/gnuman"; do export MANPATH=$mandir:$MANPATH; done
-    for mandir in "${BREW_PREFIX}/opt/"*"/share/man/man1"; do export MANPATH=$mandir:$MANPATH; done
-fi
-
 # Check and install any remaining software updates
 arrow "Checking for software updates:"
   sudo softwareupdate --install --all
